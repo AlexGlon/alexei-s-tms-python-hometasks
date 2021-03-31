8eb20b07bd960cb9f1d0b49a7d3bae946f7f7c3d
Alexei Glon <alexglon@outlook.com>
2021-03-31 15:35:56 +0300

    Task 2 syntax refactoring

--- task2.py
+++ task2.py
@@ -1,14 +1,14 @@
 balance = 0
 while True:
-    user_input = input('Enter the operations in the following format: \"ADD:__  SUB:__\" (or enter \"EXIT\" to exit):\n')
+    user_input = input('Enter an operation in the following format: \"ADD:__  SUB:__\" (or enter \"EXIT\" to exit):\n')
     if user_input == 'EXIT':
         break
     operations = user_input.split()
     try:
-      add = float(operations[0].split(':')[-1])
-      sub = float(operations[1].split(':')[-1])
+        add = float(operations[0].split(':')[-1])
+        sub = float(operations[1].split(':')[-1])
     except:
         print('Invalid input format. Please try again.')
         continue
-    balance = balance - sub + add
+    balance = balance + add - sub
     print(f"Your current balance is ${balance}.\nThanks for banking with us!\n")
