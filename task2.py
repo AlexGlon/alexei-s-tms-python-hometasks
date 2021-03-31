a02785a4d6c0a36add6aa0bb2b63cdf5b802e335
Alexei Glon <alexglon@outlook.com>
2021-03-31 15:23:19 +0300

    Task 2: endless input implemented

--- task2.py
+++ task2.py
@@ -1,11 +1,12 @@
 balance = 0
-operation = input('Enter the operations in the following format: \"ADD:__  SUB:__\":\n')
-print(operation)
-add = operation.split()
-print(add)
-addNo = add[0].split(':')[-1]
-subNo = add[1].split(':')[-1]
-print(addNo, subNo)
-sub = 46
+while True:
+    operation = input('Enter the operations in the following format: \"ADD:__  SUB:__\":\n')
+    add = operation.split()
+    print(add)
+    addNo = float(add[0].split(':')[-1])
+    subNo = float(add[1].split(':')[-1])
+    print(addNo, subNo)
+    balance = balance - subNo + addNo
+    print(f"Your current balance is ${balance}.\nThanks for banking with us!\n")
 # TODO: check if the input string follows the declared format, otherwise ask the user to enter it once again
 # TODO: "not int in __ input" exception
