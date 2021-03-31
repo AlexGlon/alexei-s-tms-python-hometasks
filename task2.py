e6d01fef2d91ca65a5be5e9cd37be130da6cd6e0
Alexei Glon <alexglon@outlook.com>
2021-03-31 15:34:51 +0300

    Task 2 complete

--- task2.py
+++ task2.py
@@ -1,12 +1,14 @@
 balance = 0
 while True:
-    operation = input('Enter the operations in the following format: \"ADD:__  SUB:__\":\n')
-    add = operation.split()
-    print(add)
-    addNo = float(add[0].split(':')[-1])
-    subNo = float(add[1].split(':')[-1])
-    print(addNo, subNo)
-    balance = balance - subNo + addNo
+    user_input = input('Enter the operations in the following format: \"ADD:__  SUB:__\" (or enter \"EXIT\" to exit):\n')
+    if user_input == 'EXIT':
+        break
+    operations = user_input.split()
+    try:
+      add = float(operations[0].split(':')[-1])
+      sub = float(operations[1].split(':')[-1])
+    except:
+        print('Invalid input format. Please try again.')
+        continue
+    balance = balance - sub + add
     print(f"Your current balance is ${balance}.\nThanks for banking with us!\n")
-# TODO: check if the input string follows the declared format, otherwise ask the user to enter it once again
-# TODO: "not int in __ input" exception
