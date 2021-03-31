13672651d703ee2906172fc3533a3e4d66175362
Alexei Glon <alexglon@outlook.com>
2021-03-31 14:49:24 +0300

    Task 1 solution expansion

--- main.py
+++ main.py
@@ -10,11 +10,21 @@ print(result)
 result = {}
 
 # Solution 2
+# кажется этот вариант можно было как-то попроще сделать
 numbers_list = numbers.split(',')
 i = 0
 while i < len(numbers_list):
     result.update({int(numbers_list[i]): int(numbers_list[i]) * int(numbers_list[i]) * int(numbers_list[i])})
     i += 1
+print(result)
+
+result = {}
+
+# Solution 3
+result = {
+    int(i): int(i)*int(i)*int(i)
+    for i in numbers.split(',')
+}
 print(result)
 
 # Тесты
