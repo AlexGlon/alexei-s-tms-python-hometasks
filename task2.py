24c4044dbe762ee10a82fe4bc667d396e75e2cc7
Alexei Glon <alexglon@outlook.com>
2021-03-31 15:04:30 +0300

    Task 2 started

---
+++ task2.py
@@ -0,0 +1,6 @@
+balance = 0
+operation = input('Enter the operations in the following format: \"ADD:__  SUB:__\":\n')
+print(operation)
+
+# TODO: check if the input string follows the declared format, otherwise ask the user to enter it once again
+# TODO: "not int in __ input" exception
