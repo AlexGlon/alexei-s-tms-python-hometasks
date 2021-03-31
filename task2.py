1976f775b6017d228e8933ee84ee3135cfdef44a
Alexei Glon <alexglon@outlook.com>
2021-03-31 15:13:46 +0300

    Task 2: input string split implemented

--- task2.py
+++ task2.py
@@ -1,6 +1,11 @@
 balance = 0
 operation = input('Enter the operations in the following format: \"ADD:__  SUB:__\":\n')
 print(operation)
-
+add = operation.split()
+print(add)
+addNo = add[0].split(':')[-1]
+subNo = add[1].split(':')[-1]
+print(addNo, subNo)
+sub = 46
 # TODO: check if the input string follows the declared format, otherwise ask the user to enter it once again
 # TODO: "not int in __ input" exception
