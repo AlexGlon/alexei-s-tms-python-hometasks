06ec2e606eff23b4adeac2a8cad9839279ae0356
Alexei Glon <alexglon@outlook.com>
2021-04-11 23:52:46 +0300

    Basic input processing added

--- main.py
+++ main.py
@@ -15,10 +15,28 @@ def get_page(page: int = 1) -> str:
 
 # TODO: user input interpreter function
 
+def input_interpreter(user_input: str) -> None:
+    """Calls a specific request handler function"""
+    if (user_input.split()[0].lower() == "show"):
+        show()
+    elif (user_input.split()[0].lower() == "urlcontains"):
+        urlcontains()
+    else:
+        print("Invalid input. Please try again.")
+
 # TODO: separate request interpreter functions
+
+
+def show():
+    pass
+
+
+def urlcontains():
+    pass
 
 
 while True:
     user_input = input("Please enter your command (or enter 'EXIT' to exit):")
-    if user_input == 'EXIT':
+    if user_input.upper() == 'EXIT':
             break
+    input_interpreter(user_input)
