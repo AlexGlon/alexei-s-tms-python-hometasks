f9ba116c3c21126a1e0a22fa3a49feb537bfc2f6
Alexei Glon <alexglon@outlook.com>
2021-04-12 12:11:20 +0300

    Show function basic logic requirements done

--- main.py
+++ main.py
@@ -16,15 +16,16 @@ def get_page(page: int = 1) -> str:
 
     print(f"Loading page {page}...")
 
-    print(requests.get(url).text)
+    # print(requests.get(url).text)
 
     return requests.get(url).text
 
+
 def input_interpreter(user_input: str) -> None:
     """Calls a specific request handler function"""
-    if (user_input.split()[0].lower() == "show"):
+    if user_input.split()[0].lower() == "show":
         show(user_input)
-    elif (user_input.split()[0].lower() == "urlcontains"):
+    elif user_input.split()[0].lower() == "urlcontains":
         urlcontains(user_input)
     else:
         print("Invalid input. Please try again.")
@@ -40,50 +41,36 @@ def show(user_input: str) -> None:
         requested = int(user_input.split()[-1])
 
     print(requested)
-    request = []
-    request.append(requested)
-    request.append(math.ceil(requested / 30))
+    request = [requested, math.ceil(requested / 30)]
     print(f"You've requested {request[0]} articles -- that'd require loading {request[1]} pages.")
 
-    # TODO: limit regex generator func
-
-    LIMIT_RE = f"class = \"rank\">{requested}."
-    # print(LIMIT_RE)
-
-    i = 1
-    while i <= request[1]:
-        received = get_page(i)
+    page_iter = 1
+    total_iter = 1
+    while page_iter <= request[1]:
+        received = get_page(page_iter)
 
         soup = BeautifulSoup(received, 'html.parser')
-        print(soup.title)
 
-        links = []
-        authors = []
-
-        #links = re.findall('title\"><a href=\"(https://.*?)\"' or 'title\"><a href=\"(.*?)\"', received)
-        #authors = re.findall('hnuser\">(.*?)<', received)
-        #links = soup.find_all('a', class_="storylink")
-        #authors = soup.find_all('a', class_="hnuser")
         headers = soup.find_all('tr', class_="athing")
         followups = soup.find_all('td', class_="subtext")
 
-        k=1
+        onpage_iter = 1
         for iteration in headers:
-            #print(f"{k} {iteration}")
-            k += 1
+            if total_iter > request[0]:
+                break
 
-        k = 1
-        for iteration in headers:
             header_processed = str(iteration)
-            followup_processed = str(followups[k-1])
-            #print(f"{header_processed}\n\n{followup_processed}")
+            followup_processed = str(followups[onpage_iter-1])
+            # print(f"{header_processed}\n\n{followup_processed}")
+
             author = ''.join(re.findall('<a class="hnuser".*?\">(.*?)</a>', followup_processed))
             link = ''.join(re.findall('href=\"(https?://.*?)\"', header_processed))
             title = ''.join(re.findall('storylink\".*?">(.*?)</a>', header_processed))
-            print(f"{k}. {author} || {title} || {link}")
-            k += 1
-        i += 1
-    pass
+            print(f"{total_iter}. Title: {title}\nURL: {link}\nAuthor: {author}")
+
+            total_iter += 1
+            onpage_iter += 1
+        page_iter += 1
 
 
 def urlcontains(user_input: str) -> None:
